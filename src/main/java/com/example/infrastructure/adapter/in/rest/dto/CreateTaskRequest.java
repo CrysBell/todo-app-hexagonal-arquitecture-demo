fd45224326a1f07ebd50e07a8acd5bfbd0b42456
Crysbell <erikacrystal95@gmai.com>
2026-09-11 10:29:11 +0200

    antes de la implementacion de TaskController que es un RestController tipico

---
+++ src/main/java/com/example/infrastructure/adapter/in/rest/dto/CreateTaskRequest.java
@@ -0,0 +1,16 @@
+package com.example.infrastructure.adapter.in.rest.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter 
+@Setter 
+public class CreateTaskRequest {
+
+    @NotBlank (message = "El titulo es obligatorio")
+    private String title;
+    private String description;
+
+}
